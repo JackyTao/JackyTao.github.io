4f19d79c7bd9a2f61f1985bec67a8c1102dc3a32
jacky zhu <zjackytao@gmail.com>
2016-07-22 09:24:15 +0800

    .

--- js/src/components/site-model.js
+++ js/src/components/site-model.js
@@ -69,6 +69,7 @@ var techData = [
         title: 'tools',
         content: [
         {'href': 'https://tinypng.com/', 'name': 'tinypng', 'category': '', 'info': ''},
+        {'href': 'http://www.draw.io/', 'name': 'drawio', 'category': '', 'info': ''},
         {'href': 'https://www.startssl.com/', 'name': 'ssl证书', 'category': '', 'info': ''},
         {'href': 'http://www.qqxiuzi.cn/daohang.htm', 'name': '各种小工具', 'category': '', 'info': ''},
         {'href': 'http://logio.org/', 'name': 'logio', 'category': '', 'info': ''},
